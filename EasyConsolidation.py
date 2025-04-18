96e9ff3a2c5d5fb6571549c36cfacd6e1f2063c6
Anthony Garrett <tony.garrett@gmail.com>
2025-04-18 08:02:44 -0400

    First Commit and file setup

---
+++ EasyConsolidation.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+# Author: Anthony Garrett
+#
+# Script to parse inventory data presented in spreadsheet format to allow for easy consolidation of similar materials to maximize free space in the warehouse.
